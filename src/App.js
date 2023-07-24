dd028f8875f434313638bc57b7bb117c809ef2fa
Chuong <chuongnh29@gmail.com>
2023-07-24 15:24:43 +0700

    Create meta tag with node server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { AppRoutes } from './AppRoutes';
-import { Header } from './Header';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AppRoutes />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ChuongNH Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
